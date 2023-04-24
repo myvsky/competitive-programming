aa388410564cf004830da73405ff87ade4b22ebb
mayevskaya <meowevskaya@gmail.com>
2023-04-24 21:20:15 +0500

    New simple problem solution added.

---
+++ A-RSO/M2K.cpp
@@ -0,0 +1,37 @@
+// Municipal stage, Krym, 2023
+
+/*
+    Given 2 strings containing only lowercase latin characters. Need to return common chars.
+    If none of common chars was found, return "epidemic".
+*/
+
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end();
+
+using namespace std;
+using i64 = long long;
+using vll = vector<long long>;
+
+void foo() {
+    string s, s1;
+    set<char> res;
+    cin >> s >> s1;
+
+    for (auto x: s) if (s1.find(x) != string::npos) res.insert(x);
+    for (auto x: res) cout << x;
+    if (res.size() == 0) cout << "epidemic\n";
+    else cout << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+
+    while (T--) foo();
+
+    return 0;
+}
--- Codeforces/TEMPLATE.cpp
+++ Codeforces/TEMPLATE.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+#define all(x) x.begin(), x.end();
+
 using namespace std;
 using i64 = long long;
 using vll = vector<long long>;
