7b0d50cf868b11bf85ff73a8c4a812396d4dc794
mayevskaya <meowevskaya@gmail.com>
2023-04-25 22:16:39 +0500

    New solution added.

---
+++ A-RSO/MK7.cpp
@@ -0,0 +1,40 @@
+// Municipal Stage, Krym, 2022-2023
+
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end();
+
+using namespace std;
+using ll = long long;
+using vll = vector<long long>;
+
+void foo() {
+    ll x1, x2, count = 0;
+    cin >> x1 >> x2;
+
+    for (x1; x1<=x2; x1++) {
+        string ss = to_string(x1);
+        bool isBeauty = false;
+        if (ss.size() == 1) { count++; continue; };
+        for (int i = 0; i < ss.size()-1; i++) {
+            // If we got it, count
+            if (ss[i]-'0' == ss[i+1]-'0'+1 || ss[i]-'0' == ss[i+1]-'0'-1) isBeauty = true;
+            // If we don't, stop it
+            else { isBeauty = false; break; }
+            }
+        if (isBeauty) count++;
+    }
+    cout << count << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+
+    while (T--) foo();
+
+    return 0;
+}
